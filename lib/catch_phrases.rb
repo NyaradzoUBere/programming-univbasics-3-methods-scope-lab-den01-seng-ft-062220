629451e63c09f3c81885c9fe1c8420321c430739
Nyaradzo Bere <nyaradzo.bere@colorado.edu>
2020-06-01 18:26:59 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,10 +1,11 @@
+$status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+
 def mario phrase = "It's-a me, Mario!"
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
   puts phrase
 end
 
 mario
 
-# def toadstool
-#   puts status
-# end
+def toadstool
+  puts status
+end
